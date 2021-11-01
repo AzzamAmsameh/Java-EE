169632ff56099e5d19ccb297b5f43b8f0b12a4bd
Azzam Abu Msameh <azzam.abumsameh@gmail.com>
2021-11-01 02:51:38 +0200

    project init

---
+++ src/com/digitanery/training/model/EntryPoint.java
@@ -0,0 +1,18 @@
+package com.digitanery.training.model;
+
+/**
+ * 
+ * @author Azzam Abu Msameh
+ *
+ */
+public class EntryPoint {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Wellcome in the first project Spring Project");
+	}
+}
